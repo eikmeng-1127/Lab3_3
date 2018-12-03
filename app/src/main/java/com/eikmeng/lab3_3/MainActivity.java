8de5eb6d2a5166ed1cc777aba67edc383aa3501d
Eik Meng <leongem-wa15@student.tarc.edu.my>
2018-12-03 18:47:44 +0800

    xd

--- app/src/main/java/com/eikmeng/lab3_3/MainActivity.java
+++ app/src/main/java/com/eikmeng/lab3_3/MainActivity.java
@@ -2,12 +2,116 @@ package com.eikmeng.lab3_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinnerAge;
+    private RadioGroup radioGroupGender;
+    private RadioButton radiobButtonMale, radioButtonFemale;
+    private CheckBox checkBoxSmoker;
+    private TextView textViewPremium;
+    private Button buttonCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerAge = findViewById(R.id.spinnerAge);
+        radioGroupGender = findViewById(R.id.radioGroupGender);
+        radiobButtonMale = findViewById(R.id.radioButtonMale);
+        radioButtonFemale = findViewById(R.id.radioButtonFemale);
+        checkBoxSmoker = findViewById(R.id.checkBoxSmoker);
+        textViewPremium = findViewById(R.id.textViewPremium);
+        buttonCalculate = findViewById(R.id.button);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+          this, R.array.age_group, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAge.setAdapter(adapter);
+        spinnerAge.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, "Position="+position,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void calculatePremium(View view){
+        float premium = 0;
+        int ageGroup;
+        ageGroup = spinnerAge.getSelectedItemPosition();
+        if(ageGroup == 0){
+            premium = 50;
+        }else if(ageGroup == 1){
+            premium = 55;
+        }else if(ageGroup == 2){
+            premium = 60;
+        }else if(ageGroup == 3){
+            premium = 70;
+        }else if(ageGroup == 4){
+            premium = 120;
+        }else if(ageGroup == 5){
+            premium = 160;
+        }else if(ageGroup == 6){
+            premium = 200;
+        }else if(ageGroup == 7){
+            premium = 250;
+        }
+
+        int gender = radioGroupGender.getCheckedRadioButtonId();
+
+        if(gender == R.id.radioButtonMale){
+            //TODO calculate extra premium for male
+            if(ageGroup == 2){
+                premium += 50;
+            }else if(ageGroup == 3){
+                premium += 100;
+            }else if(ageGroup == 4){
+                premium += 100;
+            }else if(ageGroup == 5){
+                premium += 50;
+            }
+        }
+
+        if(checkBoxSmoker.isChecked()){
+            //TODO calculate extra premium for smoker
+            if(ageGroup == 3){
+                premium += 100;
+            }else if(ageGroup == 4){
+                premium += 150;
+            }else if(ageGroup == 5){
+                premium += 150;
+            }else if(ageGroup == 6){
+                premium += 250;
+            }else if(ageGroup == 7){
+                premium += 250;
+            }
+        }
+
+        textViewPremium.setText(getString(R.string.premium) + " " + premium);
+    }
+
+    public void reset(View view){
+        spinnerAge.setSelection(-1);
+        radiobButtonMale.setChecked(false);
+        checkBoxSmoker.setChecked(false);
+        textViewPremium.setText(getString(R.string.premium));
     }
 }
